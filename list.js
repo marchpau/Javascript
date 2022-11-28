ad2c47816bbdbe55b3f1316f475b9a89df65111a
Pau March <pmarch@minsait.com>
2022-11-28 11:36:07 +0100

    Avance

--- list.js
+++ list.js
@@ -1,36 +1,38 @@
-const url = "https://rickandmortyapi.com/api/character";
-const getAllCharacters = async () => {
-  try {
-    const respuesta = await fetch(url);
-    if (respuesta.status === 200) {
-      const res = await respuesta.json();
-      const characters = res.results;
-      console.log(characters);
-      let characterList = "";
-      characters.forEach((character) => {
-        console.log(character.name);
-        characterList += `
-					<div class="character_container">
-						<img class="character_image" src="${character.image}">
-						<h3 id="${character.id}" class="character_name">${character.name}</h3>
-					</div>
-				`;
-      });
-      document.getElementById("container").innerHTML = characterList;
-      let characterLink = document.querySelectorAll("h3");
+import { getAllCharacters } from "/info.js";
 
-      for (let link of characterLink) {
-        link.addEventListener("click", () => {
-          const element = document.querySelector(".container");
-          element.style.display = "none";
-        });
-      }
-    } else {
-      console.log("Algo ha fallado");
-    }
-  } catch (error) {
-    console.log(error);
+function showHome() {
+  const homeInfo = `
+      <div class="logo">
+      <img src="https://hips.hearstapps.com/vidthumb/images/rick-y-morty-1626783132.jpg" alt="Logo Rick_and_Morty">
+      </div>
+      <div class="home_container">
+      <p class="home_info">Rick y Morty (en inglés: Rick and Morty) es una serie de televisión americana de animación 
+      para adultos creada por Justin Roiland y Dan Harmon en 2013 para Adult Swim, también se emitió en Cartoon Network. 
+      La serie sigue las desventuras de un científico, Rick Sanchez, y su fácilmente influenciable nieto, Morty, quienes 
+      pasan el tiempo entre la vida doméstica y los viajes espaciales, temporales e intergalácticos. Justin Roiland, 
+      el co-creador de la serie y Justin Roiland son los encargados de las voces principales de Morty y Rick, la serie 
+      también incluye las voces de Chris Parnell, Spencer Grammer y Sarah Chalke.</p>
+      </div>
+    `;
+  document.getElementById("home").innerHTML = homeInfo;
+  document.querySelector(".container").style.display = "none";
+}
+
+showHome();
+
+const Home = document.querySelector(".inicio");
+Home.addEventListener("click", () => {
+  document.querySelector(".container").style.display = "none";
+  document.querySelector(".home").style.display = "flex";
+  function showAlert() {
+    alert("Ya estamos aquí");
   }
-};
+  setTimeout(showAlert, 1000);
+});
 
-getAllCharacters();
+const List = document.querySelector(".lista");
+List.addEventListener("click", () => {
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".container").style.display = "flex";
+  getAllCharacters();
+});
